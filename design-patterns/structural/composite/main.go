0d7aca5bfa0ae63b814be0352e41ab70f38711ed
maciej.dobrzanski <matt950806@wp.pl>
2023-11-19 22:17:33 +0100

    Update main

--- design-patterns/structural/composite/main.go
+++ design-patterns/structural/composite/main.go
@@ -1,22 +1,29 @@
 package main
 
 func main() {
-    file1 := &File{name: "File1"}
-    file2 := &File{name: "File2"}
-    file3 := &File{name: "File3"}
+	file1 := &File{name: "File1"}
+	file2 := &File{name: "File2"}
+	file3 := &File{name: "File3"}
 
-    folder1 := &Folder{
-        name: "Folder1",
-    }
+	folder1 := &Folder{
+		name: "Folder1",
+	}
 
-    folder1.add(file1)
+	folder1.add(file1)
 
-    folder2 := &Folder{
-        name: "Folder2",
-    }
-    folder2.add(file2)
-    folder2.add(file3)
-    folder2.add(folder1)
+	folder2 := &Folder{
+		name: "Folder2",
+	}
+	folder2.add(file2)
+	folder2.add(file3)
+	folder2.add(folder1)
 
-    folder2.search("rose")
+	folder2.search("rose")
+
+	folder3 := &Folder{
+		name: "Folder3",
+	}
+
+	folder3.add(folder2)
+	folder3.search("File1")
 }
