9a3d2a339c01824914210cc01f49cb1c0ee9498b
maciej.dobrzanski <matt950806@wp.pl>
2024-08-24 23:08:41 +0200

    add numbers

--- basics/numbers.go
+++ basics/numbers.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
 
 func main() {
 	count, total := 0, 0
@@ -11,5 +14,17 @@ func main() {
 		}
 	}
 
-	fmt.Println(total / count)
+	fmt.Println(float32(total) / float32(count))
+
+	nums := []float64{2, 1, 3}
+
+	sort.Float64s(nums)
+	var median float64
+	i := len(nums) / 2
+	if len(nums)%2 == 1 {
+		median = nums[i]
+	} else {
+		median = (nums[i-1] + nums[i]) / 2
+	}
+	fmt.Println(median)
 }
