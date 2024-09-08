531a2e38cf7242354020659726a257e907f32324
maciej.dobrzanski <matt950806@wp.pl>
2024-09-08 15:47:27 +0200

    Add challenge solution for frequencies

--- basics/text.go
+++ basics/text.go
@@ -3,7 +3,10 @@ package main
 
 import (
 	"fmt"
+	"sort"
 	"strings"
+
+	orderedmap "github.com/wk8/go-ordered-map/v2"
 )
 
 var poem = `
@@ -17,6 +20,8 @@ let them sleep
 `
 
 func main() {
+	// fmt.Println("a"[0])
+
 	frequency := make(map[string]int)
 	fmt.Println(frequency)
 	for _, word := range strings.Fields(poem) {
@@ -41,4 +46,45 @@ func main() {
 		}
 	}
 	fmt.Println(maxWs, maxC)
+
+	/*
+	   Print character and frequency in percent of Rumi's poem
+	   Print in frequency descending order
+	   Assume ASCII text, ignore white space
+	*/
+
+	charFrequency := make(map[rune]int, 27)
+	sum := 0
+	// fmt.Println(string(rune(97)))
+	for _, c := range poem {
+		if (c <= 122) && (c >= 97) {
+			charFrequency[c] += 1
+			sum += 1
+		}
+	}
+
+	fmt.Println("Char frequency:")
+	for c, f := range charFrequency {
+		fmt.Println(fmt.Sprintf(" %s: %d", string(c), f))
+	}
+
+	// sorting
+	keys := make([]rune, 0, len(charFrequency))
+	for key := range charFrequency {
+		keys = append(keys, key)
+	}
+
+	sort.Slice(keys, func(i, j int) bool {
+		return charFrequency[keys[i]] < charFrequency[keys[j]]
+	})
+
+	charFrequencySorted := orderedmap.New[string, float32]()
+	for _, c := range keys {
+		charFrequencySorted.Set(string(c), float32(charFrequency[c])/float32(sum))
+	}
+
+	fmt.Println("Char frequency sorted:")
+	for pair := charFrequencySorted.Oldest(); pair != nil; pair = pair.Next() {
+		fmt.Printf("%s: %f\n", pair.Key, pair.Value)
+	}
 }
