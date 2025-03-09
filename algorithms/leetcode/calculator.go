e26b05bbe116ec0142c47e97b5d5fb5869b231d2
Matmozaur <matt950806@wp.pl>
2025-03-09 09:26:08 +0100

    update

---
+++ algorithms/leetcode/calculator.go
@@ -0,0 +1,50 @@
+// Given a string s representing a valid expression, implement a basic calculator to evaluate it, and return the result of the evaluation.
+
+import (
+	"fmt"
+	"strings"
+)
+
+func find_end(s string) int {
+    counter := 1
+    for pos, char := range s {
+        switch char {
+            case '(': counter += 1
+            case ')': counter -= 1
+        } 
+        if counter == 0 {
+            return pos
+        }
+    }
+    return -1
+}
+
+func calculate(s string) int {
+    res := 0
+    sign := 1
+    last := 0
+    s = strings.Replace(s, " ", "", -1)
+    for pos, char := range s {
+        switch char{
+            case '(': {
+                end := find_end(s[pos+1:]) + pos + 1
+                return res + sign * calculate(s[pos+1: end]) + calculate(s[end+1:])
+            }
+            case '-': {
+                sign = -1
+                last = 0
+            }
+            case '+': {
+                sign = 1
+                last = 0
+            }
+            default: {
+                x := sign * int(char - '0')
+                res += last * 9 + x
+                last = last * 10 + x
+            }
+        }
+    }
+
+    return res
+}
